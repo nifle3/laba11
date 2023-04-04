2354795faf5755bff3809605f145c82242f2f03c
nifie <kuper116rus.2005@gmail.com>
2023-04-04 20:33:12 +0300

    Add Student class

---
+++ app11/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace app11
+{
+    public class Student
+    {
+        public int StudentId { set; get; }
+        public string FirstName { set; get; }
+        public string SecondName { set; get; }
+        public DateOnly Birthday { set; get; }
+        public string Email { set; get; }
+        public int NumberPhone { set; get; }
+        public string Facility { set; get; }
+        public int Group { set; get; }
+        public int CourseOfEducation { set; get; }
+        public DateOnly DateOfEntryToEducation { set; get; }
+    }
+}
