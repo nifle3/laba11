6b4655c9f42f2a5b30aa6b80a47deab8313a93aa
nifie <kuper116rus.2005@gmail.com>
2023-04-09 22:18:38 +0300

    add pass generator

---
+++ app11/PasswordGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app11
+{
+    internal static class PasswordGenerator
+    {
+        public static string GetPassword()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+            Random rnd = new Random();
+
+            return new string(Enumerable.Repeat(chars, 12)
+            .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
--- app11/ResetPasswordForm.cs
+++ app11/ResetPasswordForm.cs
@@ -24,6 +24,7 @@ namespace app11
                 return;
 
             string email = textBox1.Text;
+            string newPassword = PasswordGenerator.GetPassword();
 
 
         }
