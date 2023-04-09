55c418c8f65a7e3e2085b66f606fa3e7d94bfcb0
nifie <kuper116rus.2005@gmail.com>
2023-04-09 22:31:41 +0300

    add email sender

---
+++ app11/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app11
+{
+    internal static class EmailSender
+    {
+        public static void SendNewPass(string email, string pass)
+        {
+            MailAddress from = new("forrandom111222@gmail.com","Artem");
+            MailAddress to = new(email);
+
+            MailMessage m = new(from, to);
+            m.Subject = "New password";
+            m.Body = $"New password - {pass}";
+            m.IsBodyHtml = true;
+
+            SmtpClient smtp = new ("", "");
+            smtp.Credentials = new NetworkCredential("forrandom111222@gmail.com", "uD15hM59X#rR");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+        }
+    }
+}
